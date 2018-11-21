357e2cafaa552ac88d778637b5631555b3a3d4e7
Christopher Pace <Christopher_pace@ymail.com>
2018-11-21 10:15:07 -0600

    check in

---
+++ answer.js
@@ -0,0 +1,12 @@
+var darray=[2,6,7];
+var newArr=[];
+
+function compare() {
+    for(i=0, i<darray.length,i++)
+        if (darray[i]<newArr[1]) {
+            newArr.append(darray[i])
+            return newArr
+        }
+}
+
+compare();
